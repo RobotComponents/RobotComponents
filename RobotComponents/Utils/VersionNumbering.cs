79d9ce955dfb3e8d4a9e2c9527f6f63d888b8e82
Arjen Deetman <info@arjendeetman.nl>
2019-12-15 14:44:36 +0100

    Version number

--- RobotComponents/Utils/VersionNumbering.cs
+++ RobotComponents/Utils/VersionNumbering.cs
@@ -8,7 +8,7 @@
         /// X.00.XXX ---> minor release(for example new functions new components...etc.)
         /// X.XX.000 ---> Bug fixes small improvements
         /// </summary>
-        public static readonly string CurrentVersion = "0.04.019";
+        public static readonly string CurrentVersion = "0.04.020";
     }
 
 }

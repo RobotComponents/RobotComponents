68d195ad1321bd620cf20146b072e5aab2bad083
arjendeetman <info@arjendeetman.nl>
2020-06-23 11:24:22 +0200

    Updated version fields

--- RobotComponents/Properties/AssemblyInfo.cs
+++ RobotComponents/Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RobotComponents")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("Robot Components")]
+[assembly: AssemblyCopyright("Copyright © 2018 - 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,15 +21,9 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("496ba2ce-d067-42b3-bd7f-d1bb429a4987")] // This will also be the Guid of the Rhino plug-in
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Version information for an assembly
+// 0.XX.XXX ---> mature release
+// X.00.XXX ---> minor release (for example new functions, new components...etc.)
+// X.XX.000 ---> bug fixes and small improvements
+[assembly: AssemblyVersion(RobotComponents.Utils.VersionNumbering.CurrentVersion)]
+[assembly: AssemblyFileVersion(RobotComponents.Utils.VersionNumbering.CurrentVersion)]

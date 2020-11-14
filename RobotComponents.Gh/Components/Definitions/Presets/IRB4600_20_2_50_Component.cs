e81a2f0b384095cf4a093a726730c9ddcfcf4f51
Arjen Deetman <arjendeetman@hotmail.com>
2020-11-14 09:58:27 +0100

    Changed exposure of IRB4600 robot preset components

--- RobotComponents.Gh/Components/Definitions/Presets/IRB4600_20_2_50_Component.cs
+++ RobotComponents.Gh/Components/Definitions/Presets/IRB4600_20_2_50_Component.cs
@@ -42,7 +42,7 @@ namespace RobotComponents.Gh.Components.Definitions.Presets
         /// </summary>
         public override GH_Exposure Exposure
         {
-            get { return GH_Exposure.secondary; }
+            get { return GH_Exposure.secondary | GH_Exposure.obscure; }
         }
 
         /// <summary>

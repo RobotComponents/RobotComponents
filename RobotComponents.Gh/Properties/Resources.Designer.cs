c52a3b92e6d73e720e4b12fe03157ac404533f8f
Arjen Deetman <arjendeetman@hotmail.com>
2021-12-28 21:43:33 +0100

    Moved .psd file paramter icons to private repo

--- RobotComponents.Gh/Properties/Resources.Designer.cs
+++ RobotComponents.Gh/Properties/Resources.Designer.cs
@@ -921,16 +921,6 @@ namespace RobotComponents.Gh.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] Parameter_Icons {
-            get {
-                object obj = ResourceManager.GetObject("Parameter_Icons", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap PathGen_Icon {

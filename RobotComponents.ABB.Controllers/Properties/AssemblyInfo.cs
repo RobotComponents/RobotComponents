cc3582a2365fee0701207accbb2de0ec5c1b7859
Arjen Deetman <arjendeetman@hotmail.com>
2022-04-03 20:54:15 +0200

    Added the project RobotComponents.ABB.Controllers (placeholder for now)

---
+++ RobotComponents.ABB.Controllers/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RobotComponents.ABB.Controllers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Robot Components")]
+[assembly: AssemblyCopyright("Copyright © 2018 - 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4d8169cc-20cf-4597-b304-331eed09278b")]
+
+// Version information for an assembly
+// 0.x.x ---> MAJOR version when you make incompatible API changes
+// x.0.x ---> MINOR version when you add functionality in a backwards compatible manner
+// x.x.0 ---> PATCH version when you make backwards compatible bug fixes
+[assembly: AssemblyVersion(RobotComponents.Utils.VersionNumbering.CurrentVersion)]
+[assembly: AssemblyFileVersion(RobotComponents.Utils.VersionNumbering.CurrentVersion)]

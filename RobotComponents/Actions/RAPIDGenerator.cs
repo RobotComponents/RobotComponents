f0e47bf98d83d5bd0f33d98dd114e2070707ef6b
Arjen Deetman <arjendeetman@hotmail.com>
2022-11-19 16:53:43 +0100

    Prep in RAPID generator for v2.0

--- RobotComponents/Actions/RAPIDGenerator.cs
+++ RobotComponents/Actions/RAPIDGenerator.cs
@@ -274,6 +274,54 @@ namespace RobotComponents.Actions
             // Closes Module
             _programModule.Add("ENDMODULE");
 
+            #region For v2.0: Adds the tooldata and wobjdata to the program module (remove SYS module)
+            /*
+            // Create tooldata
+            List<string> tooldata = new List<string>();
+
+            foreach (KeyValuePair<string, RobotTool> entry in _robotTools)
+            {
+                if (entry.Value.Name != "tool0" && entry.Value.Name != null && entry.Value.Name != "")
+                {
+                   tooldata.Add("    " + $"{entry.Value.ToRAPIDDeclaration()}");
+                }
+            }
+
+            // Create wobjdata
+            List<string> wobjdata = new List<string>();
+
+            foreach (KeyValuePair<string, WorkObject> entry in _workObjects)
+            {
+                if (entry.Value.Name != "wobj0" && entry.Value.Name != null && entry.Value.Name != "")
+                {
+                    wobjdata.Add("    " + $"{entry.Value.ToRAPIDDeclaration()}");
+                }
+            }
+
+            // Add data
+            int index = 5;
+
+            if (tooldata.Count != 0)
+            {
+                _programModule.Insert(index, "    " + "! User defined tooldata");
+                index += 1;
+                _programModule.InsertRange(index, tooldata);
+                index += tooldata.Count;
+                _programModule.Insert(index, "    ");
+                index += 1;
+            }
+
+            if (wobjdata.Count != 0)
+            {
+                _programModule.Insert(index, "    " + "! User defined wobjdata");
+                index += 1;
+                _programModule.InsertRange(index, wobjdata);
+                index += wobjdata.Count;
+                _programModule.Insert(index, "    ");
+            }
+            */
+            #endregion
+
             // Write to file
             if (_saveToFile == true)
             {

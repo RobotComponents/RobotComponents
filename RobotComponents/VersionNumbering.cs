04b18b3a74397e5eec25cbc8e520d3ca5f0a3a5a
Arjen Deetman <arjendeetman@hotmail.com>
2023-04-07 10:11:25 +0200

    Updated the version number

--- RobotComponents/VersionNumbering.cs
+++ RobotComponents/VersionNumbering.cs
@@ -6,25 +6,29 @@
 namespace RobotComponents
 {
     /// <summary>
-    /// Stores the current version number of Robot Components
+    /// Represents a class that stores the current version number of Robot Components.
     /// </summary>
     public static class VersionNumbering
     {
         /// <summary>
-        /// Gets the current version of the Robot Component Plugin. 
+        /// Gets the current version as a string.
+        /// </summary>
+        /// <remarks>
         /// Has to be manually updated each time. 
         /// 0.x.x ---> MAJOR version when you make incompatible API changes
         /// x.0.x ---> MINOR version when you add functionality in a backwards compatible manner,
-        /// x.x.0 ---> PATCH version when you make backwards compatible bug fixes
-        /// </summary>
-        public const string CurrentVersion = "2.0.2";
+        /// x.x.0 ---> BUILD version when you make backwards compatible bug fixes
+        /// </remarks>
+        public const string CurrentVersion = "2.0.3";
 
         /// <summary>
-        /// Gets the current version of the Robot Components plugin as an int. 
+        /// Gets the current version as an int. 
+        /// </summary>
+        /// <remarks>
         /// Typically used to check the version number inside the code.
         /// For internal use only. Not recommended to use. 
-        /// Used logic: major*10^6 + minor*10^3 + patch.
-        /// </summary>
-        public const int CurrentVersionAsInt = 2 * 1000000 + 0 * 1000 + 2;
+        /// Used logic: major*10^6 + minor*10^3 + build.
+        /// </remarks>
+        public const int CurrentVersionAsInt = 2 * 1000000 + 0 * 1000 + 3;
     }
 }

075abf84d6906461e68a8d88cc02eb739e0e86c9
Arjen Deetman <arjendeetman@hotmail.com>
2023-12-27 23:08:35 +0100

    Reorganized the classes for the ikfast solver

---
+++ RobotComponents.ABB/Kinematics/IkFast/Geometry/Vector6d.cs
@@ -0,0 +1,46 @@
+﻿// This file is part of Robot Components. Robot Components is licensed 
+// under the terms of GNU General Public License version 3.0 (GPL v3.0)
+// as published by the Free Software Foundation. For more information and 
+// the LICENSE file, see <https://github.com/RobotComponents/RobotComponents>.
+
+// System Libs
+using System.Collections.Generic;
+// Robot Components Libs
+using RobotComponents.ABB.Actions.Declarations;
+
+namespace RobotComponents.ABB.Kinematics.IkFast.Geometry
+{
+    /// <summary>
+    /// Represents the internal Vector6d class.
+    /// </summary>
+    internal struct Vector6d
+    {
+        public Vector6d(double x1, double x2, double x3, double x4, double x5, double x6)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.x3 = x3;
+            this.x4 = x4;
+            this.x5 = x5;
+            this.x6 = x6;
+        }
+
+        public double[] ToArray()
+        {
+            return new double[6]{ x1, x2, x3, x4, x5, x6 };
+        }
+
+        public List<double> ToList()
+        {
+            return new List<double>() { x1, x2, x3, x4, x5, x6 };
+        }
+
+        public RobotJointPosition ToRobotJointPosition()
+        {
+            return new RobotJointPosition(x1, x2, x3, x4, x5, x6);
+        }
+
+        public double x1, x2, x3, x4, x5, x6;
+    }
+}
+
---
+++ RobotComponents.ABB/Kinematics/IkFast/IkFastSolver.cs
@@ -0,0 +1,127 @@
+﻿// This file is part of Robot Components. Robot Components is licensed 
+// under the terms of GNU General Public License version 3.0 (GPL v3.0)
+// as published by the Free Software Foundation. For more information and 
+// the LICENSE file, see <https://github.com/RobotComponents/RobotComponents>.
+
+// System Libs
+// Rhin Libs
+using Rhino.Geometry;
+// Robot Components Libs
+using RobotComponents.ABB.Actions.Declarations;
+using RobotComponents.ABB.Kinematics.IkFast.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace RobotComponents.ABB.Kinematics.IkFast
+{
+    /// <summary>
+    /// Inverse kinematics for the CRB15000-5/0.95 robot (GoFa 5).
+    /// </summary>
+    /// <remarks>
+    /// This class wraps the ikfast library generated for the CRB15000-5/0.95 robot (GoFa 5). 
+    /// So far, this is the only robot supported.
+    /// </remarks>
+    internal class IkFastSolver
+    {
+        #region
+        private int _numSolutions = 0;
+        private RobotJointPosition _robotJointPosition = new RobotJointPosition();
+        private List<RobotJointPosition> _robotJointPositions = new List<RobotJointPosition>();
+        private readonly Plane _base = Plane.WorldYZ;
+
+        // Constants
+        private const double _pi = Math.PI;
+        private const double _rad2deg = 180.0 / _pi;
+        private const double _cos45 = 0.70710678118;
+        #endregion
+
+        #region constructor
+        /// <summary>
+        /// Initializes a new instance of the Inverse Kinematics class.
+        /// </summary>
+        public IkFastSolver()
+        {
+
+        }
+        #endregion
+
+        #region methods
+        private void Reset()
+        {
+            _numSolutions = 0;
+            _robotJointPositions.Clear();
+            _robotJointPosition.Reset();
+        }
+
+        /// <summary>
+        /// Computes the inverse kinematics.
+        /// </summary>
+        /// <param name="endPlane"> The robot end plane. </param>
+        public void Compute(Plane endPlane)
+        {
+            // Reset the solution
+            Reset();
+
+            // Position
+            IkFast.Geometry.Vector3d position = new IkFast.Geometry.Vector3d(endPlane.Origin);
+
+            // Orientation as quaternion
+            Rhino.Geometry.Quaternion quaternion = Rhino.Geometry.Quaternion.Rotation(_base, endPlane);
+            Rhino.Geometry.Quaternion quaternionCorrection = new Rhino.Geometry.Quaternion(_cos45, 0.0, _cos45, 0.0); // Unkown 90 degrees correction
+            Rhino.Geometry.Quaternion quaternionCorrected = quaternion * quaternionCorrection.Inverse;
+            IkFast.Geometry.Quaternion orientation = new IkFast.Geometry.Quaternion(quaternionCorrected);
+
+            RobotJointPosition robotJointPosition;
+
+            unsafe
+            {
+                Vector6d* joints_ikgen = computeInverseKinematics(ref position, ref orientation, out _numSolutions);
+
+                for (int i = 0; i < _numSolutions; i++)
+                {
+                    robotJointPosition = joints_ikgen[i].ToRobotJointPosition();
+                    robotJointPosition.Multiply(_rad2deg); // Radians to degrees
+
+                    _robotJointPositions.Add(robotJointPosition);
+                }
+            }
+
+            _robotJointPosition = _robotJointPositions.DefaultIfEmpty(new RobotJointPosition()).Last();
+        }
+        #endregion
+
+        #region properties
+        /// <summary>
+        /// Gets the latest calculated Robot Joint Position.
+        /// </summary>
+        public RobotJointPosition RobotJointPosition
+        {
+            get { return _robotJointPosition; }
+        }
+
+        /// <summary>
+        /// Gets all calculated Robot Joint Positions.
+        /// </summary>
+        public List<RobotJointPosition> RobotJointPositions
+        {
+            get { return _robotJointPositions; }
+        }
+
+        /// <summary>
+        /// Get number of computed inverse kinematics solutions.
+        /// </summary>
+        public int NumSolutions
+        {
+            get { return _numSolutions; }
+        }
+        #endregion
+
+        #region dll import
+        [DllImport("rcik.dll", EntryPoint = "computeInverseKinematics")]
+        private static unsafe extern Vector6d* computeInverseKinematics(ref IkFast.Geometry.Vector3d eePos, ref IkFast.Geometry.Quaternion eeOri, out int n_sol);
+        #endregion
+    }
+}
+

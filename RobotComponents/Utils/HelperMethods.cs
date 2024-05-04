36d960772048a1aa71446e0d5d0fbfcc5faf634c
arjendeetman <arjendeetman@hotmail.com>
2024-05-04 11:45:09 +0200

    Added a helper method that converts a rotation matrix to a plane

--- RobotComponents/Utils/HelperMethods.cs
+++ RobotComponents/Utils/HelperMethods.cs
@@ -22,13 +22,31 @@ namespace RobotComponents.Utils
             if (m.ColumnCount != 3 || m.RowCount != 3)
             {
                 Rhino.RhinoApp.WriteLine("Matrix dimensions do not match the expected size.");
+                return;
             }
-            else
+
+            Rhino.RhinoApp.WriteLine($"{m[0, 0]}, {m[0, 1]}, {m[0, 2]}");
+            Rhino.RhinoApp.WriteLine($"{m[1, 0]}, {m[1, 1]}, {m[1, 2]}");
+            Rhino.RhinoApp.WriteLine($"{m[2, 0]}, {m[2, 1]}, {m[2, 2]}");
+        }
+
+        /// <summary>
+        /// Creates a plane from a 3x3 rotation matrix.
+        /// </summary>
+        /// <param name="m"> The 3x3 rotation matrix. </param>
+        /// <returns> The plane. </returns>
+        public static Plane Matrix3x3ToPlane(Matrix m) 
+        {
+            if (m.ColumnCount != 3 || m.RowCount != 3)
             {
-                Rhino.RhinoApp.WriteLine($"{m[0, 0]}, {m[0, 1]}, {m[0, 2]}");
-                Rhino.RhinoApp.WriteLine($"{m[1, 0]}, {m[1, 1]}, {m[1, 2]}");
-                Rhino.RhinoApp.WriteLine($"{m[2, 0]}, {m[2, 1]}, {m[2, 2]}");
+                Rhino.RhinoApp.WriteLine("Matrix dimensions do not match the expected size.");
+                return Plane.Unset;
             }
+
+            Vector3d xAxis = new Vector3d(m[0, 0], m[1, 0], m[2, 0]);
+            Vector3d yAxis = new Vector3d(m[0, 1], m[1, 1], m[2, 1]);
+
+            return new Plane(new Point3d(0, 0, 0), xAxis, yAxis);
         }
     }
 }

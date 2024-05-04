58ace4489204f9e5e81ddff6c1167e07d595ab55
arjendeetman <arjendeetman@hotmail.com>
2024-05-04 11:51:40 +0200

    Updated the version number

--- RobotComponents/VersionNumbering.cs
+++ RobotComponents/VersionNumbering.cs
@@ -22,7 +22,7 @@ namespace RobotComponents
         /// x.0.x ---> MINOR version when you add functionality in a backwards compatible manner,
         /// x.x.0 ---> BUILD version when you make backwards compatible bug fixes
         /// </remarks>
-        public const string CurrentVersion = "3.0.0";
+        public const string CurrentVersion = "3.0.1";
 
         /// <summary>
         /// Gets the current version.
@@ -33,6 +33,6 @@ namespace RobotComponents
         /// x.0.x ---> MINOR version when you add functionality in a backwards compatible manner,
         /// x.x.0 ---> BUILD version when you make backwards compatible bug fixes
         /// </remarks>
-        public static Version Version = new Version(3, 0, 0);
+        public static Version Version = new Version(3, 0, 1);
     }
 }
